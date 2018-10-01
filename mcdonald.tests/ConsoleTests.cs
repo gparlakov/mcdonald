6409de288af6e250cfb09be304a82a6a65e668bb
Georgi Parlakov <Georgi.Parlakov@experian.com>
2018-10-01 09:24:56 +0300

    Fix test

--- mcdonald.tests/ConsoleTests.cs
+++ mcdonald.tests/ConsoleTests.cs
@@ -28,52 +28,63 @@ public class ConsoleTests
     public void RunAppWithoutParameters_ExpectDefaultMcdonaldVerse()
     {
         // arrange
-        var expected = string.Join("\n", new string[] { "Old MACDONALD had a farm E-I-E-I-O",
-"And on his farm he had a cow E-I-E-I-O",
-"With a moo moo here",
-"And a moo moo there",
-"Here a moo, there a moo",
-"Everywhere a moo moo",
-"Old MacDonald had a farm E-I-E-I-O",
-"",
-"Old MACDONALD had a farm E-I-E-I-O",
-"And on his farm he had a cat E-I-E-I-O",
-"With a meow meow here",
-"And a meow meow there",
-"Here a meow, there a meow",
-"Everywhere a meow meow",
-"Old MacDonald had a farm E-I-E-I-O",
-"",
-"Old MACDONALD had a farm E-I-E-I-O",
-"And on his farm he had a dog E-I-E-I-O",
-"With a bark bark here",
-"And a bark bark there",
-"Here a bark, there a bark",
-"Everywhere a bark bark",
-"Old MacDonald had a farm E-I-E-I-O",
-"",
-"Old MACDONALD had a farm E-I-E-I-O",
-"And on his farm he had a horse E-I-E-I-O",
-"With a neigh neigh here",
-"And a neigh neigh there",
-"Here a neigh, there a neigh",
-"Everywhere a neigh neigh",
-"Old MacDonald had a farm E-I-E-I-O",
-"",
-"Old MACDONALD had a farm E-I-E-I-O",
-"And on his farm he had a some chicken E-I-E-I-O",
-"With a cluck cluck here",
-"And a cluck cluck there",
-"Here a cluck, there a cluck",
-"Everywhere a cluck cluck",
-"Old MacDonald had a farm E-I-E-I-O",
-"",
-""})
-+ Environment.NewLine;
+        var expected = string.Join(Environment.NewLine, new string[] { 
+            "Old MACDONALD had a farm E-I-E-I-O",
+            "And on his farm he had a cow E-I-E-I-O",
+            "With a moo moo here",
+            "And a moo moo there",
+            "Here a moo, there a moo",
+            "Everywhere a moo moo",
+            "Old MacDonald had a farm E-I-E-I-O",
+            "",
+            "Old MACDONALD had a farm E-I-E-I-O",
+            "And on his farm he had a cat E-I-E-I-O",
+            "With a meow meow here",
+            "And a meow meow there",
+            "Here a meow, there a meow",
+            "Everywhere a meow meow",
+            "Old MacDonald had a farm E-I-E-I-O",
+            "",
+            "Old MACDONALD had a farm E-I-E-I-O",
+            "And on his farm he had a dog E-I-E-I-O",
+            "With a bark bark here",
+            "And a bark bark there",
+            "Here a bark, there a bark",
+            "Everywhere a bark bark",
+            "Old MacDonald had a farm E-I-E-I-O",
+            "",
+            "Old MACDONALD had a farm E-I-E-I-O",
+            "And on his farm he had a horse E-I-E-I-O",
+            "With a neigh neigh here",
+            "And a neigh neigh there",
+            "Here a neigh, there a neigh",
+            "Everywhere a neigh neigh",
+            "Old MacDonald had a farm E-I-E-I-O",
+            "",
+            "Old MACDONALD had a farm E-I-E-I-O",
+            "And on his farm he had a some chicken E-I-E-I-O",
+            "With a cluck cluck here",
+            "And a cluck cluck there",
+            "Here a cluck, there a cluck",
+            "Everywhere a cluck cluck",
+            "Old MacDonald had a farm E-I-E-I-O",
+            "",
+            ""})
+            + Environment.NewLine;
         // act
         mcdonald.consoleApp.Program.Main();
         // assert
         Assert.Equal(expected, testWriter.Result());
+    }
+
+    [Fact]
+    public void TestName()
+    {
+    //Given
+    
+    //When
+    
+    //Then
     }
 }
 
